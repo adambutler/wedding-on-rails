0421b5f7326e184550602769c2c2cda2e4d4d2fd
Adam Butler <adam@lab.io>
2014-11-10 12:37:53 +0000

    Init admin dashboard

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,17 @@
+class AdminController < ApplicationController
+  before_filter :authenticate
+
+  def dashboard
+    @guests = Guests.all
+  end
+
+  private
+
+  def authenticate
+    if Rails.env.production?
+      authenticate_or_request_with_http_basic do |username, password|
+        username == ENV['ADMIN_USERNAME'] && password == ENV['ADMIN_PASSWORD']
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  get "admin" => "admin#dashboard"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
