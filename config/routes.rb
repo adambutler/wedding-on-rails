4fa87d8044b405366ab4ce60f8356bc803c8ff2a
Adam Butler <adam@lab.io>
2014-11-10 12:46:14 +0000

    Add call to action if no guests exist

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   get "admin" => "admin#dashboard"
-
+  resources :guest
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
