4ccfbf7a1167b9da055fb9db25b768a959329667
Adam Butler <adam@lab.io>
2014-11-16 20:12:58 +0000

    Guest -> Guests

--- app/models/guests.rb
+++
@@ -1,2 +0,0 @@
-class Guests < ActiveRecord::Base
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
   get "admin" => "admin#dashboard"
-  resources :guest
+  resources :guests
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
