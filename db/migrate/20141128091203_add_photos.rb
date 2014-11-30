25459a7db9ecac438f1f1a937766082301aa243d
Adam Butler <adam@lab.io>
2014-11-30 15:02:47 +0000

    Delete all photo files in favour of using generator

--- db/migrate/20141128091203_add_photos.rb
+++
@@ -1,5 +0,0 @@
-class AddPhotos < ActiveRecord::Migration
-  def change
-    create_table :photos
-  end
-end
--- db/migrate/20141129145434_add_file_to_photos.rb
+++
@@ -1,5 +0,0 @@
-class AddFileToPhotos < ActiveRecord::Migration
-  def change
-    add_column :photos, :file, :string
-  end
-end
