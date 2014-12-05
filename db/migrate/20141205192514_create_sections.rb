4cef5a990489e9e6f974bdd0ffd55fc2f10b0e55
Adam Butler <adam@lab.io>
2014-12-05 19:27:04 +0000

    Add section model

---
+++ db/migrate/20141205192514_create_sections.rb
@@ -0,0 +1,11 @@
+class CreateSections < ActiveRecord::Migration
+  def change
+    create_table :sections do |t|
+      t.string :title
+      t.text :copy
+      t.integer :event_id
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141130162339) do
+ActiveRecord::Schema.define(version: 20141205192514) do
 
   create_table "events", force: true do |t|
     t.datetime "date"
@@ -58,6 +58,14 @@ ActiveRecord::Schema.define(version: 20141130162339) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "event_id"
+  end
+
+  create_table "sections", force: true do |t|
+    t.string   "title"
+    t.text     "copy"
+    t.integer  "event_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
