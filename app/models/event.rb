f93e0f5085301f761f3e2ba77795c753efeb82e8
Adam Butler <adam@lab.io>
2014-12-15 12:20:51 +0000

    create task_list on event creation

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,11 @@ class Event < ActiveRecord::Base
   has_many :guests
   has_many :photos
   has_many :sections
+  has_many :task_lists
+  has_many :tasks
   has_many :venue_photos
+
+  after_create :create_event_task_list
 
   def days_to_go
     (date.to_time - Time.now).to_i / 1.day
@@ -15,5 +19,11 @@ class Event < ActiveRecord::Base
 
   def in_past?
     date < Time.now
+  end
+
+  private
+
+  def create_event_task_list
+    TaskList.create({event_id: id})
   end
 end
