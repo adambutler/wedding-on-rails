0b0afca5913ffedd5069ecd2d28be742d64a2546
Adam Butler <adam@lab.io>
2014-12-26 15:14:25 +0000

    Add guest support

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,4 +1,14 @@
 class Guest < ActiveRecord::Base
   belongs_to :group
   belongs_to :event
+
+  after_initialize :init
+
+  def init
+    self.name  ||= "Guest"
+  end
+
+  def is_guest?
+    name.nil?
+  end
 end
