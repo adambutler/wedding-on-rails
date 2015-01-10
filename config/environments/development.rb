d08b6b01bb19adb511bf73647777e491b2805bf6
Adam Butler <adam@lab.io>
2015-01-10 17:49:07 +0000

    wedding -> hitched

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,9 +16,9 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.asset_host = "http://wedding.dev"
+  config.action_mailer.asset_host = "http://hitched.dev"
   config.action_mailer.default_url_options = {
-    host: "wedding.dev"
+    host: "hitched.dev"
   }
 
   # Print deprecation notices to the Rails logger.
